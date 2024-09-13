1d1a813d6f6407dc77a52aa965e706fdfa88ba3f
rashupk007 <ahmedrashaad@hotmail.com>
2024-09-13 18:42:51 +0530

    Updated config file

--- playwright.config.js
+++ playwright.config.js
@@ -19,9 +19,9 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 10 : 20,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
@@ -42,19 +42,19 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     screenshot: 'on',
-    video: 'on',
-    headless: false,
-    browserName: 'chromium'
+    video: 'on'
+    // headless: false,
+    // browserName: 'chromium'
   },
 
   /* Configure projects for major browsers */
-  // projects: [
-  //   {
-  //     name: 'chromium',
-  //     use: { ...devices['Desktop Chrome'],
-  //       headless: false
-  //      },
-  //   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'],
+        headless: false
+       },
+    },
 
     // {
     //   name: 'firefox',
@@ -89,7 +89,7 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  // ],
+  ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
