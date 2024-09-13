d40fdc928d18789cb7358f0d757567902f86a2ca
rashupk007 <ahmedrashaad@hotmail.com>
2024-09-13 18:48:29 +0530

    Updated config file again

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 10 : 20,
+  workers: process.env.CI ? 2 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
